4684d68df7467ffa03fed97761fbe9d24d6e122e
carolineclark <carolinejaneclark@gmail.com>
2016-06-19 17:24:38 +0100

    Added spare test

--- ten-pin-bowling/src/TenPinBowling.java
+++ ten-pin-bowling/src/TenPinBowling.java
@@ -4,4 +4,8 @@ public class TenPinBowling {
     public int getFrameScore(int i, int j) {
         return i + j;
     }
+
+    public int getSpareFrameScore(int i) {
+        return 10 + i;
+    }
 }
--- ten-pin-bowling/test/TenPinBowlingTest.java
+++ ten-pin-bowling/test/TenPinBowlingTest.java
@@ -8,4 +8,10 @@ public class TenPinBowlingTest {
         TenPinBowling tpb = new TenPinBowling();
         assertEquals(tpb.getFrameScore(2, 1), 3);
     }
+
+    @Test
+    public void aFrameThatIsASpare_isASumOfTheFramePlusTheNextRoll() {
+        TenPinBowling tpb = new TenPinBowling();
+        assertEquals(tpb.getSpareFrameScore(3), 13);
+    }
 }
