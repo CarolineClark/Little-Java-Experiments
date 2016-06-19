4528b2b2abef9cd0ecd2f938ab9bc099d1f16a95
carolineclark <carolinejaneclark@gmail.com>
2016-06-19 17:26:41 +0100

    Made function package protected

--- ten-pin-bowling/src/TenPinBowling.java
+++ ten-pin-bowling/src/TenPinBowling.java
@@ -3,11 +3,11 @@ public class TenPinBowling {
 
     private int TOTAL_FRAME_SCORE = 10;
 
-    public int getFrameScore(int i, int j) {
+    int getFrameScore(int i, int j) {
         return i + j;
     }
 
-    public int getSpareFrameScore(int i) {
+    int getSpareFrameScore(int i) {
         return TOTAL_FRAME_SCORE + i;
     }
 }
