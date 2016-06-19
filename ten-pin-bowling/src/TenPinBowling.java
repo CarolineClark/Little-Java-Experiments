5b5eeff3628858508354f8bf7ff37cd2142bd60c
carolineclark <carolinejaneclark@gmail.com>
2016-06-19 17:26:00 +0100

    Changed magic variable to private variable

--- ten-pin-bowling/src/TenPinBowling.java
+++ ten-pin-bowling/src/TenPinBowling.java
@@ -1,11 +1,13 @@
 
 public class TenPinBowling {
 
+    private int TOTAL_FRAME_SCORE = 10;
+
     public int getFrameScore(int i, int j) {
         return i + j;
     }
 
     public int getSpareFrameScore(int i) {
-        return 10 + i;
+        return TOTAL_FRAME_SCORE + i;
     }
 }
